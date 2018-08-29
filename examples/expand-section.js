38d06957d1d81169bf1c298846734ccc63a49b43
James Hanford <j_hanford@origo.com.au>
2018-08-30 09:42:48 +1000

    REFACTOR: I stopped cheating, removed jquery from expand-section example

--- examples/expand-section.js
+++ examples/expand-section.js
@@ -1,42 +1,44 @@
-$(document).ready( function() {
-
-    function sectionResizeHandler(section) {
-
-        var header = section.children('header');
-        var content = section.children('div');
-
-        if(section.hasClass('expanded')) {
-            section.css("max-height", (content.height() + header.height()));
-        } else {
-            section.css("max-height", (header.height()));
-        }
-    }
-
-    //Click event handler
-    $('section.expandable').click( function() {
-        $(this).toggleClass('expanded');
-        sectionResizeHandler($(this));
-    });
+document.addEventListener("DOMContentLoaded", function() {
     
     var observerConfig = { characterData: true, attributes: true, childList: true, subtree: true};
     
+    //Some array conversion for IE11
+    var observers = document.querySelectorAll('section.expandable');
+    observers = [].slice.call(observers);
+
+    //Create an observer for each expandable section dom node
+    observers.forEach(function(targetNode) {
     
-    //Create an observer for each expandable node
-    $('section.expandable').each( function() {
-        
-        var targetNode = $(this)[0];
         var observer = new MutationObserver(function (mutations) {
         
             observer.disconnect(); //Disconnect observer to make dom mutations
             
-            console.log("Mutation occured on section: " + $(targetNode).find('header h1').text());
-            sectionResizeHandler($(targetNode));
+            console.log("Mutation occured on section: " + targetNode.querySelector('header > h1').innerHTML);
+            sectionResizeHandler(targetNode);
             
             observer.observe(targetNode, observerConfig); //Reattach observer 
         });
-  
-        observer.observe($(this)[0], observerConfig);
-
+        observer.observe(targetNode, observerConfig);
     });  
 
 });
+
+
+//Section Resize Handler (using Max-Height for animation reasons)
+function sectionResizeHandler(section) {
+
+    var header = section.querySelector('header');
+    var content = section.querySelector('div');
+
+    if(section.classList.contains('expanded')) {
+        section.style.maxHeight = (content.offsetHeight + header.offsetHeight).toString() + "px"; 
+    } else {
+        section.style.maxHeight = (header.offsetHeight).toString() + "px";
+    }
+}
+
+//Click Event Handlers
+//Note: resizing is handled by the observer as it picks up this class change
+function expandableSectionHandler(e) {
+    e.classList.toggle('expanded');
+}
